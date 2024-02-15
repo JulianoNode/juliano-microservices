abe17e806ec7d380e04229589b5d812b3bdbf088
JulianoNode <julianonode@gmail.com>
2024-02-14 21:30:38 -0300

    JMR 01 Greeting Services * Start em MICROSERVICO *

---
+++ JMR01-greeting-service/src/main/java/com/doker/AppDocker.java
@@ -0,0 +1,13 @@
+package com.doker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDocker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDocker.class, args);
+	}
+
+}
