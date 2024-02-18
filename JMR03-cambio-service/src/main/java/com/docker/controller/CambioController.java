3ecb3697ba6ffb1847a7f628a4e6edee34817b06
JulianoNode <julianonode@gmail.com>
2024-02-18 17:40:16 -0300

    Testando portas para Cambio Service PORT=>(8000 / 8001)

--- JMR03-cambio-service/src/main/java/com/docker/controller/CambioController.java
+++ JMR03-cambio-service/src/main/java/com/docker/controller/CambioController.java
@@ -2,6 +2,8 @@ package com.docker.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import com.docker.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+	
+	@Autowired
+	private Environment environment;
 
 	// http://localhost:8000/cambio-service/5/USD/BRL
 	// @GetMapping(value = "/{5}/{USD}/{BRL}")
@@ -21,7 +26,9 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
 		
+		var port = environment.getProperty("local.server.port");
+		
 		return new Cambio(
-				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
