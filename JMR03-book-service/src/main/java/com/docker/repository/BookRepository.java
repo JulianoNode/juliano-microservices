322b4500a5215499609190a127c2e0a95a15ccc1
JulianoNode <julianonode@gmail.com>
2024-02-19 22:46:29 -0300

    Integrando o Book Service ao Banco de Dados com JPA e Spring Data * OK *

--- JMR03-book-service/src/main/java/com/docker/repository/BookRepository.java
+++ JMR03-book-service/src/main/java/com/docker/repository/BookRepository.java
@@ -1,5 +1,9 @@
 package com.docker.repository;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.docker.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+	
 }
--- JMR03-book-service/src/main/java/com/docker/web/controller/BookController.java
+++ JMR03-book-service/src/main/java/com/docker/web/controller/BookController.java
@@ -1,7 +1,5 @@
 package com.docker.web.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.docker.model.Book;
+import com.docker.repository.BookRepository;
 
 @RestController
 @RequestMapping("book-service")
@@ -18,6 +17,9 @@ public class BookController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private BookRepository repository;
+	
 	// http://localhost:8100/book-service/1/BRL
 	// http://localhost:8100/book-service/{id}/{currency}
 	@GetMapping(value = "/{id}/{currency}")	
@@ -25,10 +27,12 @@ public class BookController {
 			@PathVariable("id") Long id,
 			@PathVariable("currency") String currency ) {
 		
-		var port = environment.getProperty("local.server.port");
+		var book = repository.getReferenceById(id);
+		if (book == null) throw new RuntimeException("Book not Found");
 		
-		return new Book(1L, "Hillary Rocha", "Docker Deep Dive", new Date(),
-				Double.valueOf(13.4), currency, port); 
+		var port = environment.getProperty("local.server.port");		
+		book.setEnvironment(port);		
+		return book;
 	}
 
 }
