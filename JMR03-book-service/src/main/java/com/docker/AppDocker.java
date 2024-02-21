9c2620fc03084b4afcab15662d19c5e97c3d4f4b
JulianoNode <julianonode@gmail.com>
2024-02-20 22:06:34 -0300

    Usando o Feign REST Client para Invocar o Cambio Service * docker 108.0817 *

--- JMR03-book-service/src/main/java/com/docker/AppDocker.java
+++ JMR03-book-service/src/main/java/com/docker/AppDocker.java
@@ -2,8 +2,10 @@ package com.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AppDocker {
 
 	public static void main(String[] args) {
--- JMR03-book-service/src/main/java/com/docker/web/controller/BookController.java
+++ JMR03-book-service/src/main/java/com/docker/web/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.docker.model.Book;
+import com.docker.proxy.CambioProxy;
 import com.docker.repository.BookRepository;
 import com.docker.response.Cambio;
 
@@ -24,12 +25,33 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	@Autowired
+	private CambioProxy proxy;
+	
 	// http://localhost:8100/book-service/1/BRL
 	// http://localhost:8100/book-service/{id}/{currency}
 	@GetMapping(value = "/{id}/{currency}")	
 	public Book findBook(
 			@PathVariable("id") Long id,
-			@PathVariable("currency") String currency ) {
+			@PathVariable("currency") String currency
+			) {
+		
+		var book = repository.getReferenceById(id);
+		if (book == null) throw new RuntimeException("Book not Found");
+				
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+		
+		var port = environment.getProperty("local.server.port");
+		book.setEnvironment(port + " FEIGN");
+		book.setPrice(cambio.getConvertedValue());
+		return book;
+	}
+	
+	@GetMapping(value = "/v1/{id}/{currency}")	
+	public Book findBookV1(
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
 		
 		var book = repository.getReferenceById(id);
 		if (book == null) throw new RuntimeException("Book not Found");
@@ -40,18 +62,16 @@ public class BookController {
 		params.put("to", currency);
 		
 		var response = new RestTemplate()
-		.getForEntity("http://localhost:8000/cambio-service/"
-				+ "{amount}/{from}/{to}", 
-				Cambio.class,
-				params);
+				.getForEntity("http://localhost:8000/cambio-service/"
+						+ "{amount}/{from}/{to}", 
+						Cambio.class,
+						params);
 		
 		var cambio = response.getBody();
 		
 		var port = environment.getProperty("local.server.port");
 		book.setEnvironment(port);
 		book.setPrice(cambio.getConvertedValue());
-		
 		return book;
 	}
-
 }
