47893b65a893578f4f21213553deac64c01fb4b8
JulianoNode <julianonode@gmail.com>
2024-02-26 21:01:47 -0300

    Spring Cloud API Gateway

---
+++ JMR04-api-gateway/src/test/java/com/docker/DockerTests.java
@@ -0,0 +1,13 @@
+package com.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
