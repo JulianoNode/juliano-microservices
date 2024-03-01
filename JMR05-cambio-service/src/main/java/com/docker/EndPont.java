d336547273ec2ecb813d49529b5ab5ca419db36e
JulianoNode <julianonode@gmail.com>
2024-02-29 21:21:33 -0300

    JMR05-cambio-service

---
+++ JMR05-cambio-service/src/main/java/com/docker/EndPont.java
@@ -0,0 +1,15 @@
+package com.docker;
+
+public class EndPont {
+	
+	public void name() {
+		URL1();
+	}
+
+	private void URL1() {
+		System.out.println(" Usanso os End Ponts URL1 \n"
+						 + " http://localhost:8000/cambio-service/5/USD/BRL \n");
+		
+	}
+
+}
