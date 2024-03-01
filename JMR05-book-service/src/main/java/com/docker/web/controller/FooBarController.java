dee2d45e6ca8bc1843d1e88d7be6e3d36099890f
JulianoNode <julianonode@gmail.com>
2024-03-01 19:10:15 -0300

    Conhecendo as Features de Circuit Breaker do Resilience4j

--- JMR05-book-service/src/main/java/com/docker/web/controller/FooBarController.java
+++ JMR05-book-service/src/main/java/com/docker/web/controller/FooBarController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("book-service")
@@ -16,16 +16,21 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class); 
 	
 	@GetMapping("/foo-bar")
-	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
 	
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
-		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		var response = new RestTemplate()
+				.getForEntity("http://localhost:8080/foo-bar", String.class);
 		//return "Foo-Bar!!!";
 		return response.getBody();
 	}
 	
 	public String fallbackMethod(Exception ex) {
-		return "fallbackMethod foo-bar!!!";
+		String MSN = "\n\tfallbackMethod foo-bar Error Juliano !!! Seguir o Comando abaixo\n C:\\Windows\\System32> while (1) {curl http://localhost:8765/book-service/foo-bar; sleep 1} \n"; 
+		
+		return MSN;
 	}
+
 }
