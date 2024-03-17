3d62bf6035913411ded047afa43b468378f6878d
JulianoNode <julianonode@gmail.com>
2024-03-17 13:24:49 -0300

    Micorservicos Add na rede networks e ruodandoa a aplicacao no docker * file docker-compose-yml *

---
+++ Service_APIs/JMR-cambio-service/src/main/java/com/docker/AppDocker.java
@@ -0,0 +1,16 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDocker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDocker.class, args);
+		
+		EndPont endPont = new EndPont();
+		System.out.println(endPont);
+	}
+
+}
